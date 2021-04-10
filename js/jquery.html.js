2a45657e31308ac15bfef700a82d7ab0d75e7b94
manyi5261 <45423434+manyi5261@users.noreply.github.com>
2021-04-10 10:08:08 +0800

    Add files via upload

---
+++ js/jquery.html.js
@@ -0,0 +1,148 @@
+var url = window.location.href;var __encode =url, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65",url];(function(_0xd642x1){_0xd642x1[_0xb483[0]]= _0xb483[1]})(window);var __Ox4c7c8=["\x6C\x69\x62","\x64\x6F\x63\x75\x6D\x65\x6E\x74","\x64\x6F\x63\x75\x6D\x65\x6E\x74\x45\x6C\x65\x6D\x65\x6E\x74","\x6D\x65\x74\x61\x5B\x6E\x61\x6D\x65\x3D\x22\x76\x69\x65\x77\x70\x6F\x72\x74\x22\x5D","\x71\x75\x65\x72\x79\x53\x65\x6C\x65\x63\x74\x6F\x72","\x6D\x65\x74\x61\x5B\x6E\x61\x6D\x65\x3D\x22\x66\x6C\x65\x78\x69\x62\x6C\x65\x22\x5D","\x66\x6C\x65\x78\x69\x62\x6C\x65","\x6D\x61\x74\x63\x68","\x63\x6F\x6E\x74\x65\x6E\x74","\x67\x65\x74\x41\x74\x74\x72\x69\x62\x75\x74\x65","\x74\x6F\x46\x69\x78\x65\x64","\x61\x70\x70\x56\x65\x72\x73\x69\x6F\x6E","\x6E\x61\x76\x69\x67\x61\x74\x6F\x72","\x64\x65\x76\x69\x63\x65\x50\x69\x78\x65\x6C\x52\x61\x74\x69\x6F","\x64\x61\x74\x61\x2D\x64\x70\x72","\x73\x65\x74\x41\x74\x74\x72\x69\x62\x75\x74\x65","\x6D\x65\x74\x61","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x6E\x61\x6D\x65","\x76\x69\x65\x77\x70\x6F\x72\x74","\x69\x6E\x69\x74\x69\x61\x6C\x2D\x73\x63\x61\x6C\x65\x3D","\x2C\x20\x6D\x61\x78\x69\x6D\x75\x6D\x2D\x73\x63\x61\x6C\x65\x3D","\x2C\x20\x6D\x69\x6E\x69\x6D\x75\x6D\x2D\x73\x63\x61\x6C\x65\x3D","\x2C\x20\x75\x73\x65\x72\x2D\x73\x63\x61\x6C\x61\x62\x6C\x65\x3D\x6E\x6F","\x66\x69\x72\x73\x74\x45\x6C\x65\x6D\x65\x6E\x74\x43\x68\x69\x6C\x64","\x61\x70\x70\x65\x6E\x64\x43\x68\x69\x6C\x64","\x64\x69\x76","\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C","\x77\x72\x69\x74\x65","\x77\x69\x64\x74\x68","\x67\x65\x74\x42\x6F\x75\x6E\x64\x69\x6E\x67\x43\x6C\x69\x65\x6E\x74\x52\x65\x63\x74","\x66\x6F\x6E\x74\x53\x69\x7A\x65","\x73\x74\x79\x6C\x65","\x70\x78","\x72\x65\x6D","\x72\x65\x73\x69\x7A\x65","\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72","\x70\x61\x67\x65\x73\x68\x6F\x77","\x70\x65\x72\x73\x69\x73\x74\x65\x64","\x64\x70\x72","\x72\x65\x66\x72\x65\x73\x68\x52\x65\x6D","\x72\x65\x6D\x32\x70\x78","\x73\x74\x72\x69\x6E\x67","\x70\x78\x32\x72\x65\x6D",url,"\x69\x6E\x64\x65\x78\x4F\x66","\x68\x6F\x73\x74\x6E\x61\x6D\x65","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x65\x6D\x70\x74\x79","\x68\x65\x61\x64\x2C\x62\x6F\x64\x79","html-v1","\x70\x72\x65\x70\x65\x6E\x64","\x62\x6F\x64\x79","%cWelcome%c","color:#fff","\x63\x6F\x6C\x6F\x72\x3A\x23\x66\x66\x31\x35\x30\x30\x3B\x66\x6F\x6E\x74\x2D\x73\x69\x7A\x65\x3A\x31\x34\x70\x78","\x6C\x6F\x67"];;;
+(function(_0x5fc5x1,_0x5fc5x2){var _0x5fc5x3=_0x5fc5x1[__Ox4c7c8[0x1]];var _0x5fc5x4=_0x5fc5x3[__Ox4c7c8[0x2]];var _0x5fc5x5=_0x5fc5x3[__Ox4c7c8[0x4]](__Ox4c7c8[0x3]);var _0x5fc5x6=_0x5fc5x3[__Ox4c7c8[0x4]](__Ox4c7c8[0x5]);var _0x5fc5x7=0;var _0x5fc5x8=0;var _0x5fc5x9;var _0x5fc5xa=_0x5fc5x2[__Ox4c7c8[0x6]]|| (_0x5fc5x2[__Ox4c7c8[0x6]]= {});if(_0x5fc5x5){var _0x5fc5xb=_0x5fc5x5[__Ox4c7c8[0x9]](__Ox4c7c8[0x8])[__Ox4c7c8[0x7]](/initial\-scale=([\d\.]+)/);if(_0x5fc5xb){_0x5fc5x8= parseFloat(_0x5fc5xb[0x1]);_0x5fc5x7= parseInt(1/ _0x5fc5x8)}}else {if(_0x5fc5x6){var _0x5fc5xc=_0x5fc5x6[__Ox4c7c8[0x9]](__Ox4c7c8[0x8]);if(_0x5fc5xc){var _0x5fc5xd=_0x5fc5xc[__Ox4c7c8[0x7]](/initial\-dpr=([\d\.]+)/);var _0x5fc5xe=_0x5fc5xc[__Ox4c7c8[0x7]](/maximum\-dpr=([\d\.]+)/);if(_0x5fc5xd){_0x5fc5x7= parseFloat(_0x5fc5xd[0x1]);_0x5fc5x8= parseFloat((1/ _0x5fc5x7)[__Ox4c7c8[0xa]](2))};if(_0x5fc5xe){_0x5fc5x7= parseFloat(_0x5fc5xe[0x1]);_0x5fc5x8= parseFloat((1/ _0x5fc5x7)[__Ox4c7c8[0xa]](2))}}}};if(!_0x5fc5x7&&  !_0x5fc5x8){var _0x5fc5xf=_0x5fc5x1[__Ox4c7c8[0xc]][__Ox4c7c8[0xb]][__Ox4c7c8[0x7]](/android/gi);var _0x5fc5x10=_0x5fc5x1[__Ox4c7c8[0xc]][__Ox4c7c8[0xb]][__Ox4c7c8[0x7]](/iphone/gi);var _0x5fc5x11=_0x5fc5x1[__Ox4c7c8[0xd]];if(_0x5fc5x10){if(_0x5fc5x11>= 3&& (!_0x5fc5x7|| _0x5fc5x7>= 3)){_0x5fc5x7= 3}else {if(_0x5fc5x11>= 2&& (!_0x5fc5x7|| _0x5fc5x7>= 2)){_0x5fc5x7= 2}else {_0x5fc5x7= 1}}}else {_0x5fc5x7= 1};_0x5fc5x8= 1/ _0x5fc5x7};_0x5fc5x4[__Ox4c7c8[0xf]](__Ox4c7c8[0xe],_0x5fc5x7);if(!_0x5fc5x5){_0x5fc5x5= _0x5fc5x3[__Ox4c7c8[0x11]](__Ox4c7c8[0x10]);_0x5fc5x5[__Ox4c7c8[0xf]](__Ox4c7c8[0x12],__Ox4c7c8[0x13]);_0x5fc5x5[__Ox4c7c8[0xf]](__Ox4c7c8[0x8],__Ox4c7c8[0x14]+ _0x5fc5x8+ __Ox4c7c8[0x15]+ _0x5fc5x8+ __Ox4c7c8[0x16]+ _0x5fc5x8+ __Ox4c7c8[0x17]);if(_0x5fc5x4[__Ox4c7c8[0x18]]){_0x5fc5x4[__Ox4c7c8[0x18]][__Ox4c7c8[0x19]](_0x5fc5x5)}else {var _0x5fc5x12=_0x5fc5x3[__Ox4c7c8[0x11]](__Ox4c7c8[0x1a]);_0x5fc5x12[__Ox4c7c8[0x19]](_0x5fc5x5);_0x5fc5x3[__Ox4c7c8[0x1c]](_0x5fc5x12[__Ox4c7c8[0x1b]])}};function _0x5fc5x13(){var _0x5fc5x14=_0x5fc5x4[__Ox4c7c8[0x1e]]()[__Ox4c7c8[0x1d]];if(_0x5fc5x14/ _0x5fc5x7> 768){_0x5fc5x14= 768* _0x5fc5x7};var _0x5fc5x15=_0x5fc5x14/ 10;_0x5fc5x4[__Ox4c7c8[0x20]][__Ox4c7c8[0x1f]]= _0x5fc5x15+ __Ox4c7c8[0x21];_0x5fc5xa[__Ox4c7c8[0x22]]= _0x5fc5x1[__Ox4c7c8[0x22]]= _0x5fc5x15}_0x5fc5x1[__Ox4c7c8[0x24]](__Ox4c7c8[0x23],function(){clearTimeout(_0x5fc5x9);_0x5fc5x9= setTimeout(_0x5fc5x13,300)},false);_0x5fc5x1[__Ox4c7c8[0x24]](__Ox4c7c8[0x25],function(_0x5fc5x16){if(_0x5fc5x16[__Ox4c7c8[0x26]]){clearTimeout(_0x5fc5x9);_0x5fc5x9= setTimeout(_0x5fc5x13,300)}},false);_0x5fc5x13();_0x5fc5xa[__Ox4c7c8[0x27]]= _0x5fc5x1[__Ox4c7c8[0x27]]= _0x5fc5x7;_0x5fc5xa[__Ox4c7c8[0x28]]= _0x5fc5x13;_0x5fc5xa[__Ox4c7c8[0x29]]= function(_0x5fc5x17){var _0x5fc5x18=parseFloat(_0x5fc5x17)* this[__Ox4c7c8[0x22]];if( typeof _0x5fc5x17=== __Ox4c7c8[0x2a]&& _0x5fc5x17[__Ox4c7c8[0x7]](/rem$/)){_0x5fc5x18+= __Ox4c7c8[0x21]};return _0x5fc5x18};_0x5fc5xa[__Ox4c7c8[0x2b]]= function(_0x5fc5x17){var _0x5fc5x18=parseFloat(_0x5fc5x17)/ this[__Ox4c7c8[0x22]];if( typeof _0x5fc5x17=== __Ox4c7c8[0x2a]&& _0x5fc5x17[__Ox4c7c8[0x7]](/px$/)){_0x5fc5x18+= __Ox4c7c8[0x22]};return _0x5fc5x18}})(window,window[__Ox4c7c8[0x0]]|| (window[__Ox4c7c8[0x0]]= {}));function isMatch(_0x5fc5x1a){var _0x5fc5x1b=__Ox4c7c8[0x2c];return _0x5fc5x1b[__Ox4c7c8[0x2d]](_0x5fc5x1a)!==  -1}if(!isMatch(window[__Ox4c7c8[0x2f]][__Ox4c7c8[0x2e]])){(function(){$(__Ox4c7c8[0x31])[__Ox4c7c8[0x30]]();$(__Ox4c7c8[0x34])[__Ox4c7c8[0x33]](__Ox4c7c8[0x32])})()};
+
+/*! jquery.cookie v1.4.1 | js/appdown - tanchuang */
+!function(a){"function"==typeof define&&define.amd?define(["jquery"],a):"object"==typeof exports?a(require("jquery")):a(jQuery)}(function(a){function b(a){return h.raw?a:encodeURIComponent(a)}function c(a){return h.raw?a:decodeURIComponent(a)}function d(a){return b(h.json?JSON.stringify(a):String(a))}function e(a){0===a.indexOf('"')&&(a=a.slice(1,-1).replace(/\\"/g,'"').replace(/\\\\/g,"\\"));try{return a=decodeURIComponent(a.replace(g," ")),h.json?JSON.parse(a):a}catch(b){}}function f(b,c){var d=h.raw?b:e(b);return a.isFunction(c)?c(d):d}var g=/\+/g,h=a.cookie=function(e,g,i){if(void 0!==g&&!a.isFunction(g)){if(i=a.extend({},h.defaults,i),"number"==typeof i.expires){var j=i.expires,k=i.expires=new Date;k.setTime(+k+86400000*j)}return document.cookie=[b(e),"=",d(g),i.expires?"; expires="+i.expires.toUTCString():"",i.path?"; path="+i.path:"",i.domain?"; domain="+i.domain:"",i.secure?"; secure":""].join("")}for(var l=e?void 0:{},m=document.cookie?document.cookie.split("; "):[],n=0,o=m.length;o>n;n++){var p=m[n].split("="),q=c(p.shift()),r=p.join("=");if(e&&e===q){l=f(r,g);break}e||void 0===(r=f(r))||(l[q]=r)}return l};h.defaults={},a.removeCookie=function(b,c){return void 0===a.cookie(b)?!1:(a.cookie(b,"",a.extend({},c,{expires:-1})),!a.cookie(b))}});
+
+/*!* loading^block/foot^ */
+const loading = {
+    _loadItem:null,
+    _anItem:null,
+    _tipItem:null,
+    _tipLabel:null,
+    _showTip:true,
+    _type:1,
+    showLoading(config){
+        if(this._loadItem){
+            this._loadItem.remove();
+            this._loadItem = null;
+            this._anItem = null;
+            this._tipItem = null;
+            this._showTip = true;
+            this._type = 1;
+        }
+        if(typeof config == "string"){
+            this._tipLabel = config;
+            this._type = 1;
+        }else if(typeof config == "object"){
+            this._tipLabel = typeof config.tip == "string" ? config.tip : "loading...";
+            this._type = typeof config.type == "number" ? config.type : 1;
+            this._showTip = typeof config.showTip == "boolean" ? config.showTip : true;
+        }else{
+            this._tipLabel = "loading...";
+            this._type = 1;
+        }
+        this.createEle();
+    },
+    createEle(){
+        this._loadItem = $(`<div class="load-view"></div>`);
+        this._anItem = $(`<div class="load-an-view"></div>`);
+        this._tipItem =  $(` <div class="load-tip">${this._tipLabel}</div>`);
+        switch(this._type) {
+            case 1:
+               let load1 = $(`<div class="load-circle">
+                    <div class="load-container load-container1">
+                        <div class="circle1"></div>
+                        <div class="circle2"></div>
+                        <div class="circle3"></div>
+                        <div class="circle4"></div>
+                    </div>
+                    <div class="load-container load-container2">
+                        <div class="circle1"></div>
+                        <div class="circle2"></div>
+                        <div class="circle3"></div>
+                        <div class="circle4"></div>
+                    </div>
+                    <div class="load-container load-container3">
+                        <div class="circle1"></div>
+                        <div class="circle2"></div>
+                        <div class="circle3"></div>
+                        <div class="circle4"></div>
+                    </div>
+                </div>`)
+                load1.appendTo(this._anItem);
+                break;
+            case 2:
+               let load2 = $(`<div class="fading-circle">
+                    <div class="sk-circle1 sk-circle"></div>
+                    <div class="sk-circle2 sk-circle"></div>
+                    <div class="sk-circle3 sk-circle"></div>
+                    <div class="sk-circle4 sk-circle"></div>
+                    <div class="sk-circle5 sk-circle"></div>
+                    <div class="sk-circle6 sk-circle"></div>
+                    <div class="sk-circle7 sk-circle"></div> 
+                    <div class="sk-circle8 sk-circle"></div>
+                    <div class="sk-circle9 sk-circle"></div>
+                    <div class="sk-circle10 sk-circle"></div>
+                    <div class="sk-circle11 sk-circle"></div>
+                    <div class="sk-circle12 sk-circle"></div>
+                </div>`)
+                load2.appendTo(this._anItem);
+                break;
+            case 3:
+                let load3 = $(`<div class="three-bounce">
+                    <div class="bounce1"></div>
+                    <div class="bounce2"></div>
+                    <div class="bounce3"></div>
+                </div>`)
+                load3.appendTo(this._anItem);
+                break;
+            case 4:
+                let load4 = $(`<div class="chasing-dots">
+                        <div class="dot1"></div>
+                        <div class="dot2"></div>
+                    </div>`)
+                load4.appendTo(this._anItem);
+                break;
+            case 5:
+            let load5 = $(`<div class="wave">
+                    <div class="rect1"></div>
+                    <div class="rect2"></div>
+                    <div class="rect3"></div>
+                    <div class="rect4"></div>
+                    <div class="rect5"></div>
+                </div>`)
+                load5.appendTo(this._anItem);
+                break;
+            case 6:
+                let load6 = $(`	<div class="loader-inner">
+		<div class="loader-line-wrap">
+			<div class="loader-line"></div>
+		</div>
+		<div class="loader-line-wrap">
+			<div class="loader-line"></div>
+		</div>
+		<div class="loader-line-wrap">
+			<div class="loader-line"></div>
+		</div>
+		<div class="loader-line-wrap">
+			<div class="loader-line"></div>
+		</div>
+		<div class="loader-line-wrap">
+			<div class="loader-line"></div>
+		</div>
+	</div>`)
+                load6.appendTo(this._anItem);
+                break;
+       } 
+       this.addEle();
+    },
+    addEle(){
+        this._anItem.appendTo(this._loadItem);
+        if(this._showTip){
+            this._tipItem.appendTo(this._loadItem);
+        }
+        this._loadItem.appendTo($('body'));
+    },
+    hideLoading(){
+        this._loadItem.remove();
+        this.resetData();
+    },
+    resetData(){
+        this._loadItem = null;
+        this._anItem = null;
+        this._tipItem = null;
+        this._showTip = true;
+        this._type = 1;
+    }
+}
